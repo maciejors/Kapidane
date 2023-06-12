5fc2f0a542460e8955574a2d7c17301c130a419d
maciejors <maciej.orslowski@gmail.com>
2023-06-12 21:26:57 +0200

    fix moving avg

--- tests/reports/moving_average_nightscount.sql
+++ tests/reports/moving_average_nightscount.sql
@@ -1,22 +1,22 @@
 USE [kapidane_dwh]
-GO;
+GO
 
-CREATE OR ALTER VIEW Test_MovingAvg AS
+CREATE OR ALTER VIEW Test_MovingAvg_Nights_Temp AS
 SELECT f.YearKey, 
-	o.CountryName AS OriginCountryName,
-	AVG(f2.NightsSpentCount) AS MovingAverage
+	SUM(f.NightsSpentCount) AS NightsSpentCount
 FROM Fact_TripsNights f
-JOIN Fact_TripsNights f2 ON f.DestinationCountryKey = f2.DestinationCountryKey AND f.OriginCountryKey = f2.OriginCountryKey
-    AND f.YearKey - f2.YearKey BETWEEN 0 AND 2
 JOIN Dim_Geography o ON f.OriginCountryKey = o.CountryKey
 JOIN Dim_Geography d ON f.DestinationCountryKey = d.CountryKey
 WHERE o.CountryName = 'Germany' -- filters applied on page
 	AND d.CountryName IN ('Austria', 'Belgium', 'Croatia', 'Czechia', 'Denmark', 'Italy', 'Switzerland')
-GROUP BY f.YearKey, o.CountryName
+GROUP BY f.YearKey, o.CountryName;
 GO
 
 
--- Moving Average test
-SELECT * FROM Test_MovingAvg
-WHERE YearKey >= 2013 AND YearKey <= 2020
-ORDER BY YearKey;
+-- Moving Avg test:
+SELECT f2.YearKey + 2 AS [Year], AVG(f.NightsSpentCount) AS NightsCountMovingAvg
+FROM Test_MovingAvg_Nights_Temp AS f
+JOIN Test_MovingAvg_Nights_Temp AS f2 
+	ON f.YearKey - f2.YearKey BETWEEN 0 AND 2
+GROUP BY f2.YearKey;
+
